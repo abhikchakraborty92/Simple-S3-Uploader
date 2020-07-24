88e87942d62055b85784065af28ef1d5ded95800
abhikchakraborty92 <abhik.chakraborty92@gmail.com>
2020-07-25 01:17:17 +0530

    Update uploader.py
    Added comments for necessary functions

--- uploader.py
+++ uploader.py
@@ -6,12 +6,12 @@ import time
 from multiprocessing import Process
 from threading import Thread
 
-processes = []
-file_threads = []
+processes = []      # Processes are stored in this list
 
-start = time.perf_counter()
+start = time.perf_counter() # Starting counter to track time
 
 print('***S3 FILE UPLOADER***\n')
+
 # Reading the file for getting the details to files to upload on S3
 filelist = None
 try:
@@ -26,7 +26,7 @@ try:
 except:
     print('No file to read')
 
-
+# This function is used to log the details regarding the s3 uploads
 def log_df_uploader(log_tuple):
     templist = []
     templist.append(log_tuple)
@@ -42,6 +42,7 @@ def log_df_uploader(log_tuple):
 # For this AWS CLI has to be installed and a relevant profile with required permissions have to be created
 s3 = boto3.client('s3')
 
+# This function is for uploading S3 files
 def upload_s3_file(filename,fileaddress,s3bucket,s3_subfolder):
     error = 'No Error Found'
     start_time = datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')
@@ -49,20 +50,21 @@ def upload_s3_file(filename,fileaddress,s3bucket,s3_subfolder):
         s3.upload_file(fileaddress,s3bucket,s3_subfolder)
         result = 'SUCCESS'
         end_time = datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')
-        print('\t[FILE UPLOAD ALERT:] %s upload to the SUBFOLDER %s inside the bucket %s SUCCESSFUL\n'%(str(filename),str(s3_subfolder),str(s3bucket)))
+        print('\t[FILE UPLOAD SUCCESSFUL:] FILE: %s | SUBFOLDER: %s | BUCKET: %s\n'%(str(filename),str(s3_subfolder),str(s3bucket)))
     except EnvironmentError as e:
         result = 'FAILURE'
         error = e
         #print(error)
         end_time = datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')
-        print('\t[FILE UPLOAD ALERT:] %s upload to the SUBFOLDER %s inside the bucket %s FAILED\n'%(str(filename),str(s3_subfolder),str(s3bucket)))
+        print('\t[FILE UPLOAD FAILED:] FILE: %s | SUBFOLDER: %s | BUCKET: %s\n'%(str(filename),str(s3_subfolder),str(s3bucket)))
     
     log_tuple = (filename,fileaddress,s3bucket, s3_subfolder,start_time,end_time,result,error)
     log_df_uploader(log_tuple)
 
     return result
 
-
+# This function handles S3 files and folders. Whenever we have a folder to upload, 
+# it gathers the contents of the folder and creates multiple threads to upload them almost parallel
 def upload_to_s3(filename,folderaddress,s3bucket,s3_subfolder):
     result = None 
     if os.path.isdir(folderaddress)==True:
@@ -72,27 +74,28 @@ def upload_to_s3(filename,folderaddress,s3bucket,s3_subfolder):
         contents = os.listdir(folderaddress)
         for item in contents:
             itemaddress = os.path.join(folderaddress,item)
-            if os.path.isdir(itemaddress):
-                #result = upload_to_s3(item,itemaddress,s3bucket,s3_subfolder+'/'+item)
+            if os.path.isdir(itemaddress): # if it is a directory
+                
+                # Creating multiple threads for file contents
                 th = Thread(target=upload_to_s3, args=[item,itemaddress,s3bucket,s3_subfolder+'/'+item])
                 th.start()
-                file_threads.append(th)
-            else:
-                #result = upload_s3_file(item,itemaddress,s3bucket,s3_subfolder+'/'+item)
+                
+            else: # if it is a file
+                
+                # Creating multiple threads for file contents
                 th = Thread(target=upload_to_s3, args=[item,itemaddress,s3bucket,s3_subfolder+'/'+item])
                 th.start()
-                file_threads.append(th)
+                
     else:
         result = upload_s3_file(filename,folderaddress,s3bucket,s3_subfolder)
     
-    # for file_thd in file_threads:
-    #     file_thd.join()
     
     return result
 
 
 if __name__ == '__main__':
-    # Looping through filelist
+    
+    # Looping through filelist csv
     for row,index in filelist.iterrows():
         fileaddress = os.path.join(filelist['filepath'][row],filelist['filename'][row])
 
@@ -101,8 +104,10 @@ if __name__ == '__main__':
         else:
             filelist['s3_subfolder_path'][row] = str(os.path.join(filelist['s3_subfolder_path'][row],filelist['filename'][row])).replace('\\','/')
 
+
+# For every row in the filelist file, we are creating a process and executing them at the same time
         thd = Process(target=upload_to_s3, args=[filelist['filename'][row],fileaddress,filelist['s3bucket'][row],filelist['s3_subfolder_path'][row]])
-        #upload_to_s3(filelist['filename'][row],fileaddress,filelist['s3bucket'][row],filelist['s3_subfolder_path'][row])
+        
         thd.start()
         processes.append(thd)
 
